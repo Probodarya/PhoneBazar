4bb8038531146b5c15fa9484ea7539098865ed56
Prince Bodarya <princebodarya23@gmail.com>
2026-02-24 10:03:10 +0530

    sign up page

---
+++ phonebazar/core/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+class UserSignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        # Removed password1 and password2 from fields as UserCreationForm handles them
+        fields = ['email', 'role']
+        widgets = {
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control custom-input',
+                'placeholder': 'Enter your Email'
+            }),
+            'role': forms.Select(attrs={
+                'class': 'form-control custom-input'
+            }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Apply Cashify styling to the automatically generated password fields
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control custom-input',
+                'placeholder': f'Enter {field.replace("_", " ").title()}'
+            })
