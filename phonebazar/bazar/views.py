89fa399a8fe6057f5c5a6173ccda50be9738ec35
Prince Bodarya <princebodarya23@gmail.com>
2026-02-27 09:49:56 +0530

    decoraters

--- phonebazar/bazar/views.py
+++ phonebazar/bazar/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .decorators import role_required
+
 # Create your views here.
-@login_required(login_url="login") #check in core.urls.py login name should exist..
+@role_required(allowed_roles=["Buyer"]) #check in core.urls.py login name should exist..
 def BuyerDashboardView(request):
-    return render(request,"bazar/buyer_dashboard.html")
+    return render(request,"bazar/buyer/buyer_dashboard.html")
 
-@login_required(login_url="login")
+@role_required(allowed_roles=["Seller"])
 def SellerDashboardView(request):
-    return render(request,"bazar/seller_dashboard.html")
+    return render(request,"bazar/seller/seller_dashboard.html")
 
-@login_required(login_url="login")
+@role_required(allowed_roles=["Retailer"])
 def RetailerDashboardView(request):
-    return render(request,"bazar/retailer_dashboard.html")
+    return render(request,"bazar/retailer/retailer_dashboard.html")
